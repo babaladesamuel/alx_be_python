a7627cf7b156c439f12ffddc6e6e5247063b5449
babalade samuel <babaladesamuel1@gmail.com>
2025-06-03 11:49:52 +0100

    i

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,22 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Print multiplication table
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
